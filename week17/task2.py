d2595505115a40e17b9dcf71c3c2989a12fd644f
SpideErik <erikozh@gmail.com>
2024-02-17 18:31:55 +0300

    task2

---
+++ week17/task2.py
@@ -0,0 +1,25 @@
+# Список доступных товаров
+available = ['бананы', 'сосиски', 'колбаса', 'огурцы', 'апельсины', 'гречка', 'рис', 'хлеб', 'масло']
+
+
+def check_available(name: str):
+    if name in available:
+        return True
+    print(f'{name} не продается!')
+    return False
+
+
+print('Доступные товары:')
+print(available)
+
+n = int(input('Введите кол-во покупок>'))
+goods = []
+for i in range(1, n+1):
+    while True:
+        words = input(f'Введите {i}-й товар и кол-во через пробел>').split()
+        if len(words) == 2 and words[1].isdecimal() and check_available(words[0]):
+            break
+    goods.append([words[0], int(words[1])])
+    print('Покупка добавлена в список')
+    print(words)
+print(goods)
