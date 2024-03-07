c46ef3e3b9bc54ab66725525b332c7fe8fcdd24f
SpideErik <erikozh@gmail.com>
2024-03-07 18:16:26 +0300

    task1

---
+++ week20_2/task1.py
@@ -0,0 +1,16 @@
+ban_list = []
+while word := input('Введите запрещенное слово>'):
+    ban_list.append(word.lower())
+    print(f'{word} - добавлено')
+print('Запрещенные слова:', ban_list, sep='\n')
+
+s = input('Предложение>')
+if len(s) <= 10:
+    raise ValueError('Слишком короткое предложение')
+
+words = s.lower().split()
+for i, word in enumerate(words):
+    if word in ban_list:
+        words[i] = 'пииииюююю'
+
+print(' '.join(words))
