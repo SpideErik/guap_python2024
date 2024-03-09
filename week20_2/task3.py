7e8c2af8869b86d35cbc80b4a4691c887f377443
SpideErik <erikozh@gmail.com>
2024-03-09 21:46:00 +0300

    с регулярными выражениями

--- week20_2/task3.py
+++ week20_2/task3.py
@@ -1,19 +1,26 @@
+import re
+
+
 def calc(s):
-    for op in '+-*/':
-        operand = s.split(op)
-        if len(operand) != 2:
-            continue
-        for i in range(2):
-            operand[i] = float(operand[i])
-        if op == '+':
-            return sum(operand)
-        if op == '-':
-            return operand[0] - operand[1]
-        if op == '*':
-            return operand[0] * operand[1]
-        if op == '/':
-            return operand[0] / operand[1]
-    raise ValueError('Неверное количество операндов')
+    parts = re.split(r"([\+\-\*\/])", s)
+    if len(parts) != 3:
+        raise ValueError('Неверное количество операндов')
+    try:
+        operand = [float(parts[0]), float(parts[2])]
+    except ValueError:
+        raise ValueError('Операции только над числами')
+    op = parts[1]
+
+    if op == '+':
+        return sum(operand)
+    if op == '-':
+        return operand[0] - operand[1]
+    if op == '*':
+        return operand[0] * operand[1]
+    if op == '/':
+        if operand[1] == 0:
+            raise ValueError("деление на 0")
+        return operand[0] / operand[1]
 
 
 while s := input('Выражение>').replace(' ', '').replace(',', '.'):
