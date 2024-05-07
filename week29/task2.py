e7aa769d3fb68d81668991c57e4c90b1eb226cb1
SpideErik <erikozh@gmail.com>
2024-05-07 18:39:59 +0300

    task2 fix

--- week29/task2.py
+++ week29/task2.py
@@ -3,6 +3,6 @@ import re
 
 s = input('Введите строку с датой>')
 dates = []
-for i in re.findall(r'\d{2}\D\d{2}\D\d{2,4}', s):
-    dates.append(i[:2] + '.' + i[3:5] + '.' + i[6:])
+for i in re.findall(r'\d{2}\D\d{2}\D\d{4}|\d{2}\D\d{2}\D\d{2}', s):
+    dates.append(re.sub(r'\D','.', i))
 print(dates)
